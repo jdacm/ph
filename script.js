7b6b9952b0a00d38b59fbdbecae883774b3fd987
jdacm <148639676+jdacm@users.noreply.github.com>
2023-11-19 17:23:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -29,16 +29,4 @@ const typed = new Typed(".typing-text", {
   
   setInterval(rotateRectangle, 2000);
 
-<script>
-window.onload = function () {
-  var span = document.createElement('span');
 
-  span.className = 'fa';
-  span.style.display = 'none';
-  document.body.insertBefore(span, document.body.firstChild);
-  
-  alert(window.getComputedStyle(span, null).getPropertyValue('font-family'));
-    
-  document.body.removeChild(span);
-};
-</script>
